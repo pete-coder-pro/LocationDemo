d316ef12be9a3b3d46e7b198761ed79ba4b5024a
Peter Trost <trost@intervall.io>
2023-07-02 20:26:23 +0200

    Initial Commit

---
+++ LocationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationDemo
+//
+//  Created by Peter Trost on 02.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
